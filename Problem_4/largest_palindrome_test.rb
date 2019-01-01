590fe5d8166d42a9b4587c5d2dbbc005c3a28de8
Brentwood Reid <bwoodreid@gmail.com>
2019-01-01 12:13:43 -0800

    Add additional palindrome test

--- Problem_4/largest_palindrome_test.rb
+++ Problem_4/largest_palindrome_test.rb
@@ -6,6 +6,8 @@ class LargestPalindromeTest < Minitest::Test
     assert_equal true, is_palindrome(1001)    
     assert_equal true, is_palindrome(101)   
     assert_equal true, is_palindrome(10101)   
+    assert_equal false, is_palindrome(12)    
+    assert_equal false, is_palindrome(1234)    
     assert_equal false, is_palindrome(12345)    
 	end
 end
