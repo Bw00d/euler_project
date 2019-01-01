caba576044e16e3f954384d7603b369e744f26ea
Brentwood Reid <bwoodreid@gmail.com>
2019-01-01 12:11:36 -0800

    Write is_palindrome method

--- Problem_4/largest_palindrome.rb
+++ Problem_4/largest_palindrome.rb
@@ -1,3 +1,20 @@
-# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+# A palindromic number reads the same both ways. The largest palindrome made 
+# from the product of two 2-digit numbers is 9009 = 91 × 99.
 
 # Find the largest palindrome made from the product of two 3-digit numbers.
+
+def is_palindrome(num)
+  result = true
+  arr = num.to_s.split("")
+  (arr.length / 2).times do
+    if arr.pop != arr.shift
+      result = false
+      break
+    end
+  end
+  result
+end
+
+def largest_palindrome
+  
+end
---
+++ Problem_4/largest_palindrome_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require_relative 'largest_palindrome'
+
+class LargestPalindromeTest < Minitest::Test
+	def test_is_palindrome
+    assert_equal true, is_palindrome(1001)    
+    assert_equal true, is_palindrome(101)   
+    assert_equal true, is_palindrome(10101)   
+    assert_equal false, is_palindrome(12345)    
+	end
+end
